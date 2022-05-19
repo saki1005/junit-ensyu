7a0b882474851da3795d0ceb982b788322f4ac6e
saki <sakiranbo39@gamil.com>
2022-05-19 11:10:08 +0900

    commit 1-2

---
+++ src/test/java/junit/tutorial/ex01/e01/StringUtilsTest.java
@@ -0,0 +1,47 @@
+package junit.tutorial.ex01.e01;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class StringUtilsTest {
+
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+	}
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+
+	@Test
+	void test() {
+		String test1 = StringUtils.toSnakeCase("aaa");
+		assertEquals("aaa", test1, "Not yet implemented");
+	}
+
+	@Test
+	void test2() {
+		String test1 = StringUtils.toSnakeCase("HelloWorld");
+		assertEquals("hello_world", test1, "Not yet implemented");
+	}
+
+	@Test
+	void test3() {
+		String test1 = StringUtils.toSnakeCase("practiceJunit");
+		assertEquals("practice_junit", test1, "Not yet implemented");
+	}
+
+}
---
+++ src/test/java/junit/tutorial/ex01/e02/CalculatorTest.java
@@ -0,0 +1,34 @@
+package junit.tutorial.ex01.e02;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class CalculatorTest {
+
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+	}
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+
+	@Test
+	void test() {
+		assertThrows(IllegalArgumentException.class, () -> Calculator.divide(1, 0));
+	}
+
+}
